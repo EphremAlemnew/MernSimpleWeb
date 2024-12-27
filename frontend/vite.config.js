dd11ba60a80adf34b21741e0acb8b759e5ee1b51
EphremAlemnew <ephremalemnew@gmail.com>
2024-12-27 15:06:28 +0300

    vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     watch: {
       usePolling: true,
     },
-    proxy:{
-      "/api":{
-        target: "http://localhost:5000"
-      }
-    }
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+      },
+    },
   },
- 
-})
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase the size limit to 1MB
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Create a separate chunk for vendor libraries
+          }
+        },
+      },
+    },
+  },
+});
